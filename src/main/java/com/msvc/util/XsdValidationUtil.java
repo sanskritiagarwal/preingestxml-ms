d4318f21029a86845e583527bc7a33651b4a9c0b
Sanskriti <sanskritiagarwal@users.noreply.github.com>
2018-07-23 00:17:35 -0700

    generate xsd from xml

--- src/main/java/com/msvc/util/XsdValidationUtil.java
+++ src/main/java/com/msvc/util/XsdValidationUtil.java
@@ -1,9 +1,18 @@
 package com.msvc.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.xml.XMLConstants;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import javax.xml.validation.Schema;
@@ -11,23 +20,40 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
 import org.xml.sax.SAXException;
+import org.wiztools.xsdgen.ParseException;
+import org.wiztools.xsdgen.XsdGen;
 
 public class XsdValidationUtil {
 
 	public static void main(String args[]) {
 		
-		String xml="E:\\NewWorkspace\\preingestxml-ms\\XmlMergeResult.xml";
-		String xsd="E:\\NewWorkspace\\preingestxml-ms\\src\\main\\resources\\SIEMember.xsd";
+		String xml="E:\\NewWorkspace\\preingestxml-ms\\src\\main\\resources\\SIEMember1.xml";
+		String xsd="E:\\NewWorkspace\\preingestxml-ms\\src\\main\\resources\\SIE_Member_onlineoutput.xsd";
 		
 		if(validateXMLSchema(xsd,xml))
 		{
-			System.out.println("XML is valid");
+			System.out.println(xml +"XML is valid against "+xsd);
 		}
 		else
 		{
-			System.out.println("XML is not valid ");
+			System.out.println(xml +"XML is not valid against "+xsd);
 		}
 		
+		
+		File file = new File(xml);
+		File xsdFile = new File("E:\\NewWorkspace\\preingestxml-ms\\src\\main\\resources\\SIE_Member_javaoutput.xsd");
+        try {
+        	XsdGen xsdObj=null;
+			if(!xsdFile.exists())
+        		if(xsdFile.createNewFile())
+        		{
+        			xsdObj = new XsdGen().parse(file);
+					xsdObj.write(new FileOutputStream(xsdFile));
+        		}
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -50,4 +76,22 @@ public class XsdValidationUtil {
 	      return true;
 		
 	   }
+	
+	public static String formatXSD(String xsdString) {
+	    try {
+	 
+	        Source xmlInput = new StreamSource(new StringReader(xsdString));
+	        StringWriter stringWriter = new StringWriter();
+	        StreamResult xmlOutput = new StreamResult(stringWriter);
+	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        transformerFactory.setAttribute("indent-number", 2);
+	        Transformer transformer = transformerFactory.newTransformer();
+	        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	        transformer.transform(xmlInput, xmlOutput);
+	 
+	        return xmlOutput.getWriter().toString();
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	}
 }
